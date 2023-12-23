4aa53a6e01d522787e188d671ea1a3873c9149d5
Nicu Taban <nictbndeveloper@gmail.com>
2023-12-23 18:52:50 +0200

    Adds integration test; Adds auditing for the beer entity

---
+++ src/test/java/com/example/spring6reactive/repositories/BeerRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.spring6reactive.repositories;
+
+import com.example.spring6reactive.config.DatabaseConfig;
+import com.example.spring6reactive.domain.Beer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataR2dbcTest
+@Import(DatabaseConfig.class)
+class BeerRepositoryTest {
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void saveNewBeer() {
+        beerRepository.save(getTestBeer()).subscribe(System.out::println);
+    }
+
+    Beer getTestBeer() {
+        return Beer.builder()
+                .beerName("Space Dust")
+                .beerStyle("IPA")
+                .price(BigDecimal.TEN)
+                .quantityOnHand(12)
+                .upc("123213")
+                .build();
+    }
+}
