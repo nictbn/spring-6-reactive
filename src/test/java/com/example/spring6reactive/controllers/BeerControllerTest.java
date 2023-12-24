92bb6b7ed0c896c9f1666c2a3eab16c5a407ec47
Nicu Taban <nictbndeveloper@gmail.com>
2023-12-24 17:52:23 +0200

    Tests get beer by id

--- src/test/java/com/example/spring6reactive/controllers/BeerControllerTest.java
+++ src/test/java/com/example/spring6reactive/controllers/BeerControllerTest.java
@@ -1,24 +1,39 @@
 package com.example.spring6reactive.controllers;
 
+import com.example.spring6reactive.model.BeerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static com.example.spring6reactive.controllers.BeerController.BEER_PATH;
+import static com.example.spring6reactive.controllers.BeerController.BEER_PATH_ID;
+
 @SpringBootTest
 @AutoConfigureWebTestClient
 class BeerControllerTest {
 
+    public static final String CONTENT_TYPE = "Content-type";
+    public static final String APPLICATION_JSON = "application/json";
     @Autowired
     WebTestClient webTestClient;
 
     @Test
     void testListBeers() {
-        webTestClient.get().uri(BeerController.BEER_PATH)
+        webTestClient.get().uri(BEER_PATH)
                 .exchange()
                 .expectStatus().isOk()
-                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectHeader().valueEquals(CONTENT_TYPE, APPLICATION_JSON)
                 .expectBody().jsonPath("$.size()").isEqualTo(3);
+    }
+
+    @Test
+    void testGetById() {
+        webTestClient.get().uri(BEER_PATH_ID, 1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals(CONTENT_TYPE, APPLICATION_JSON)
+                .expectBody(BeerDto.class);
     }
 }
